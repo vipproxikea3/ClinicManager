f44222794a710cab21ba5cb8f111890e1bdf7d89
Dong Quang Quy <quangquya2@gmail.com>
2020-11-17 21:18:02 +0700

    create class PatientDTO.cs

---
+++ ClinicManager/Models/DataTransferObject/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManager.Models.DataTransferObject
+{
+    public class DataProvider
+    {
+        private static DataProvider _instant;
+        public static DataProvider Instant
+        {
+            get
+            {
+                if (_instant == null)
+                    _instant = new DataProvider();
+                return _instant;
+            }
+            set
+            {
+                _instant = value;
+            }
+
+        }
+        public ClinicManagerEntities DB { get; set; }
+        private DataProvider()
+        {
+            DB = new ClinicManagerEntities();
+        }
+    }
+}
---
+++ ClinicManager/Models/DataTransferObject/PatientDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManager.Models.DataTransferObject
+{
+    public class PatientDTO
+    {
+        #region POST
+        public void CreatePatient(string Name, string DayOfBirth, bool Gender, string IdentityCardNumber)
+        {
+            string[] eOfD = DayOfBirth.Split('-');
+            DateTime dOfB = new DateTime(Int32.Parse(eOfD[0]), Int32.Parse(eOfD[1]), Int32.Parse(eOfD[2]));
+
+            DataProvider.Instant.DB.Patients.Add(new Patient() { Name = Name, DateOfBirth = dOfB, Gender = Gender, IdentityCardNumber = IdentityCardNumber });
+            DataProvider.Instant.DB.SaveChanges();
+        }
+        #endregion
+
+        #region GET
+        public List<Patient> GetPatients()
+        {
+            return DataProvider.Instant.DB.Patients.ToList();
+        }
+
+        public Patient GetPatientById(int id)
+        {
+            return DataProvider.Instant.DB.Patients.Where(x => x.IdPatient == id).FirstOrDefault();
+        }
+
+        public Patient GetPatientByIdentityCardNumber(string number)
+        {
+            return DataProvider.Instant.DB.Patients.Where(x => x.IdentityCardNumber == number).FirstOrDefault();
+        }
+        #endregion
+
+        #region PUT
+        public void UpdatePatient(int IdPatient, string Name, string DayOfBirth, bool Gender)
+        {
+            Patient patient = DataProvider.Instant.DB.Patients.Where(x => x.IdPatient == IdPatient).FirstOrDefault();
+            patient.Name = Name;
+
+            string[] eOfD = DayOfBirth.Split('-');
+            DateTime dOfB = new DateTime(Int32.Parse(eOfD[0]), Int32.Parse(eOfD[1]), Int32.Parse(eOfD[2]));
+            patient.DateOfBirth = dOfB;
+
+            patient.Gender = Gender;
+            DataProvider.Instant.DB.SaveChanges();
+        }
+        #endregion
+    }
+}
